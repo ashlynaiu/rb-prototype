18046ba4aee0e8d9f270ef08fa14ed760624b308
pbaunach <pbaunach@gmail.com>
2015-10-20 14:41:06 -0700

    Leads and Opptys lists

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,25 +1,14 @@
-var firstNameArray = ['Cindy', 'Henry', 'Charles', 'Andrea', 'Carey', 'Cristopher', 'Dan', 'Deborah', 'Frank', 'Jack', 'Janice', 'Kay', 'Michael', 'Nancy', 'Rachel', 'Randy', 'Robert', 'Stefanie', 'Peter', 'Steve', 'Stuart', 'Victoria', 'Lyn','Sally','Judy','Sophia','Aiden','Emma','Jackson','Olivia','Ethan','Isabella','Liam','Ava','Mason','Lily','Noah','Zoe','Lucas','Chloe','Jacob','Mia','Jayden','Madison','Jack','Emily','Logan','Ella','Ryan','Madelyn','Caleb','Benjamin','Abigail','Aubrey','William','Addison','Avery','Alexander','Layla','Elijah','Hailey','Matthew','Amelia','Dylan','Hannah','James','Owen','Conner','Brayden','Carter','Charlotte','Kaitlyn','Harper','Kaylee','Sophie','Mackenzie','Peyton','Riley','Grace','Brooklyn','Sarah','Aaliyah','Anna','Arianna','Ellie','Natalie','Isabelle','Lillian','Evelyn','Elizabeth','Lyla','Lucy','Claire','Makayla','Kylie','Audrey','Landon','Joshua','Luke','Daniel','Gabriel','Nicholas','Nathan','Oliver','Andrew','Gavin','Cameron','Eli','Max','Isaac', 'Evan','Samuel','Grayson','Tyler','Zachary','Wyatt','Joseph','Therese', 'Meda', 'Chadrick', 'Adan', 'Halle', 'Susie', 'Mabelle', 'Frederic', 'Lilla', 'Jovan', 'Melvin', 'Jeramie', 'Austyn', 'Celestine', 'Macy', 'Oscar', 'Ardith', 'Kelly', 'Kraig', 'Lisandro', 'Brendan', 'Drew', 'Carlos', 'Jayde', 'Ida', 'Arnulfo', 'Richard', 'Robyn', 'Jeanette', 'Euna', 'Carlos', 'Carlos', 'Lydia', 'Remington', 'Cooper', 'Coleman', 'Vincenzo', 'Angela', 'Lillian', 'Madaline', 'Mercedes', 'Marie', 'Margret', 'Pierre', 'Francisca', 'Angelica', 'Lavinia', 'Tanya', 'Katelin', 'Sabina', 'Finn', 'Daija', 'Lois', 'Bria', 'Eino', 'Cassidy', 'Ellie', 'Martina', 'Rhett', 'Jordy', 'Angelina', 'Jaycee', 'Gabe', 'Chad', 'Clair', 'Torey', 'Johnpaul', 'Toby', 'Damien', 'Rex', 'Foster', 'Joyce', 'Daija', 'Damion', 'Chyna', 'Alexandria', 'Elisha', 'Elian', 'Natalie', 'Stephen', 'Jeffrey', 'Garrick', 'Madisen', 'Delphine', 'Coby', 'Christop', 'Glen', 'Desmond', 'Bo', 'Johnnie', 'Cecile', 'Trace', 'Shaina', 'Samson', 'Angelita', 'Mafalda', 'Garrett', 'Teresa', 'Nakia', 'Darlene', 'Clint', 'Mac', 'Jazmyne', 'Kathryne', 'Liza', 'Nathanael', 'Amie', 'Donna', 'Leta', 'Monte', 'Maximillia', 'Foster', 'Marty', 'Jordan', 'Elvera', 'Tyreek', 'Sherwood', 'Marcel', 'Herminia', 'Ocie', 'Monserrat', 'Jose', 'Lola', 'Dandre', 'Linnea', 'Albert', 'Berenice', 'Sydnie', 'Leora', 'Kristofer', 'Jonatan', 'Jaron', 'Jaiden', 'Joany', 'Nathanial', 'Hattie', 'Ethyl', 'Daija', 'Arlo', 'Elinore', 'Alexis', 'Raymond', 'Davon', 'Vern', 'Annette', 'Kathryne', 'Lavon', 'Carrie', 'Kelsi', 'Vinnie', 'Leo', 'Kaley', 'Gideon', 'Gudrun', 'Percy', 'Antonia', 'Elvie', 'Barbara', 'Martina', 'Ayden', 'Catharine', 'Ayana', 'Orland', 'Layne', 'Joshua', 'Savanna', 'Janick', 'Marvin', 'Liliana', 'Alanna', 'Chase', 'Arno', 'Iliana', 'Emery', 'Emmalee', 'Mathias', 'Agustin', 'Audie', 'Coty', 'Dorris', 'Jordyn', 'Cleta', 'Delphia', 'Bernita', 'Jaquelin', 'Petra', 'Bruce', 'Laurie', 'Cydney', 'Charity', 'Houston', 'Nigel', 'Reina', 'Kelvin', 'Kelsi', 'Ruth', 'Bailee', 'Delilah', 'Annabel', 'Cali', 'Liliane', 'Justina', 'Creola', 'Freddie', 'Arielle', 'Leo', 'Arlie', 'Kale', 'Emory', 'Jovany', 'Easter', 'Rubie', 'Ashleigh', 'Dario', 'Braden', 'Effie', 'Delphia', 'Joel', 'Teresa', 'Lucile', 'Alene', 'Maxwell', 'Rex', 'Amelia', 'Lenny', 'Carlee', 'Jenifer', 'Desiree', 'Nakia', 'Rickie', 'Pablo', 'Arnaldo', 'Noemi', 'Afton', 'Flavie', 'Andres', 'Taya', 'Scot', 'Ona', 'Cristal', 'Vito', 'Matilda', 'Cleve', 'Coty', 'Rogelio', 'Isaac', 'Jadon', 'Berniece', 'Isabel', 'Brennon', 'Sallie', 'Jordyn', 'Pearline', 'Columbus', 'Lura', 'Easter', 'Kayleigh', 'Eveline', 'Cristina', 'Clement', 'Talon', 'Bell', 'Melyssa', 'Otto', 'Woodrow', 'Solon', 'Lillie', 'Ezra', 'Kiarra', 'Carmen', 'Carolina', 'Precious', 'Giovanny', 'Owen', 'Garnet', 'Aleen', 'Linwood', 'Ronaldo', 'Cordie', 'Jarred', 'Richie', 'Jarrod', 'Marlon', 'Rhett', 'Keith', 'Maurice', 'Coty', 'Guido', 'Manuela', 'Alek', 'Edmond', 'Rosanna', 'Cheyanne', 'Ariane', 'Marlene', 'Donnell', 'Delphine', 'Kendra', 'Sage', 'Macy', 'Sylvia', 'Boyd', 'Isabelle', 'Roberta', 'Hunter', 'Myrtie', 'Sharon', 'Kacey', 'Toney', 'Turner', 'Eliane', 'Austin', 'Billy', 'Cathrine', 'Jerod', 'Micheal', 'Tyreek', 'Alek', 'Fannie', 'Berniece', 'Blanche', 'Osvaldo', 'Adrienne', 'Riley', 'Colten', 'Ozzie', 'Carole', 'Magdalena', 'Shaun', 'Shany', 'Remington', 'Mariam', 'Brant', 'Mariano', 'Judson', 'Kenya', 'Evangeline', 'Brenden', 'Colton', 'Dejon', 'Kay', 'Raquel', 'Vinnie', 'Myra', 'Veda', 'Joelle', 'Delphine', 'Kellie', 'Edd', 'Lee', 'Misty', 'Ally', 'Salvatore', 'Eleazar', 'Loren', 'Ambrose', 'Kirstin', 'Heath', 'Tamia', 'Jon', 'Leslie', 'Keegan', 'Orlo', 'Haley', 'Loyal', 'Tillman', 'Esta', 'Sam', 'Roscoe', 'Katrina', 'Kyler', 'Brianne', 'Krystal', 'Stevie', 'Kiana', 'Jamaal', 'Agnes', 'Grayce', 'Abdul', 'Jerry', 'Easton', 'Alex', 'Nikki', 'Johanna', 'Otha', 'Deontae', 'Dwight', 'Brionna', 'Julius', 'Deborah', 'Alyson', 'Emilia', 'Pablo', 'Osborne', 'Titus', 'Jedediah', 'Giovani', 'Josefa', 'Major', 'Arno'];
-var lastNameArray = ['Medici', 'Bowden', 'Con', 'Underwood', 'Hickey','Star','Mainwaring','Rund','Delaney','Jones','Muir','Swanson','Baxter','Linstrom','Hosie','Katz','Boyle','Wood','Matteson','Montgomery','Jackson','Baker','Murry','Smith','Adams','Cord','Johnson','Williams','Brown','Miller','Davis','Garcia','Rodriguez','Wilson','Martinez','Anderson','Taylor','Thomas','Hernandez','Moore','Martin','White','Lopez','Lee','Harris','Lewis','Walker','Perez','King','Hall','Hill','Campbell','Mitchell','Howard','Ward','Foster','Myers','Reyes','Fisher','Coleman','Simmons','Patterson','Ramos','Kim','Gutierrez','Jenkins','Ortiz','Bridges','Abbot','Boone','Sweeney','York','Farrell','Skinner','Franco','Combes','Whitaker','Morissette', 'Hilll', 'Bergnaum', 'Reynolds', 'Huels', 'Emard', 'Funk', 'Leannon', 'Cartwright', 'Keeling', 'Dickinson', 'Shanahan', 'Harvey', 'Schmitt', 'Beier', 'Hand', 'Jacobs', 'Leannon', 'Prohaska', 'Jaskolski', 'Windler', 'Schroeder','Satterfield', 'Reichel', 'Weissnat', 'Hermiston', 'Toy', 'McKenzie', 'Keeling', 'Dare', 'Rippin', 'Deckow', 'Wyman', 'Jenkins', 'Gibson', 'Boyer', 'Reichel', 'Deckow', 'Howell', 'Mosciski', 'Beier', 'Fadel', 'Bartell', 'Ritchie', 'Effertz', 'Cummings', 'Pfannerstill', 'Hermiston', 'Weber', 'Zboncak', 'Gerlach', 'Borer', 'Lehner', 'Daugherty', 'Hauck', 'Metz', 'Bogisich', 'Schuppe', 'Lind', 'Fahey', 'Spencer', 'Langosh', 'Kassulke', 'Hackett', 'Raynor', 'Price', 'Champlin', 'Hagenes', 'Greenfelder', 'Nicolas', 'Hoppe', 'Reilly', 'Abernathy', 'Zemlak', 'Schowalter', 'Daugherty', 'Koelpin', 'Howe', 'Homenick', 'Blanda', 'Luettgen', 'Steuber', 'Vandervort', 'Grady', 'Crona', 'Hoeger', 'Walsh', 'Hirthe', 'Schneider', 'Douglas', 'Kuphal', 'Larson', 'Torp', 'Rau', 'Walsh', 'Conroy', 'Torp', 'Schumm', 'Lakin', 'Mayer', 'Farrell', 'Fahey', 'Collins', 'Wisozk', 'Prohaska', 'Bayer', 'McKenzie', 'White', 'Kuhic', 'Rodriguez', 'Reinger', 'Deckow', 'Wilkinson','Halvorson', 'Heidenreich', 'Davis', 'Donnelly', 'Wunsch', 'Gibson', 'Bednar', 'Upton', 'Grimes', 'Frami', 'Hane', 'Rice', 'Schowalter', 'Bergnaum', 'Barrows', 'Corwin', 'Kuphal', 'Dooley', 'Bailey', 'Prosacco', 'Corwin', 'Baumbach', 'Boyle', 'Toy', 'Kertzmann', 'Kulas', 'Schamberger', 'Welch', 'Lang', 'Koss', 'Zieme', 'Becker', 'Jast', 'Mayert', 'Kihn', 'Schneider', 'Parisian', 'Daniel', 'Smith', 'Steuber', 'Welch', 'Dickens', 'Rosenbaum', 'Fahey', 'Ledner', 'Bogisich', 'Collier', 'Yundt', 'Champlin', 'Pagac', 'Gorczany', 'Kohler', 'Beier', 'Rice', 'Rau', 'Kulas', 'Farrell', 'Little', 'Stark', 'Stiedemann', 'Waters', 'Ryan', 'Skiles', 'Wolff', 'Harber', 'Emard', 'Hoppe', 'Graham', 'Anderson', 'Pfeffer', 'Labadie', 'Nikolaus', 'Herman', 'Stokes', 'West', 'Jacobs', 'Hermiston', 'Dickinson', 'Rau', 'Howell','Muller', 'Kihn', 'Morar', 'Batz', 'Parker', 'Stark', 'Dibbert', 'Gerlach', 'Pollich',  'Bartell', 'Hoppe', 'Fritsch', 'Watsica', 'Crooks', 'Pfeffer', 'Pfeffer', 'Lang', 'Gerhold', 'Herzog', 'Feeney', 'Lindgren', 'Jacobi', 'Dickinson', 'McCullough', 'Corkery', 'Lubowitz', 'Schmitt', 'Brown', 'Heller', 'Trantow', 'Blick', 'Schoen', 'Bergnaum', 'Bosco', 'Howell', 'Baumbach', 'Kassulke', 'Heaney', 'Johnson', 'Weissnat', 'Stehr', 'Lynch', 'Lemke', 'Leffler', 'Schumm', 'Rippin', 'Howell', 'Schroeder', 'Friesen', 'Carter', 'Ferry', 'Heaney', 'Smitham', 'Gorczany', 'Batz', 'Mills', 'Bosco', 'Hauck', 'Smith', 'McCullough', 'Zboncak', 'Nolan', 'Howe', 'Hamill', 'Nolan', 'Fadel', 'Littel', 'Ritchie', 'Lind', 'Torphy', 'Lockman', 'Cummings', 'McLaughlin', 'Durgan', 'Gorczany', 'Beatty', 'Carter', 'Walter', 'Hoeger', 'Johns', 'Terry', 'Kihn', 'Corkery', 'Franecki', 'Nitzsche', 'Koelpin', 'Parker', 'Mayer', 'Dickens', 'Schoen', 'Tremblay', 'Balistreri', 'Donnelly', 'Schimmel', 'Prohaska', 'Stroman', 'Hayes', 'Dare', 'Gerhold', 'Christiansen', 'Christiansen', 'Tromp', 'Hand', 'Waelchi', 'Rosenbaum', 'Heathcote', 'Conn', 'Rice', 'Morissette', 'Renner', 'Schultz', 'Farrell', 'Fay', 'Braun', 'Bartell', 'Miller', 'Lemke', 'Ward', 'Nienow', 'Reilly', 'Torphy', 'Goyette', 'Ankunding', 'Swaniawski', 'Daniel', 'Berge', 'Abbott', 'Ritchie', 'Jakubowski', 'Lebsack', 'Pacocha', 'Hintz', 'Denesik', 'Runolfsdottir', 'Padberg', 'Marks', 'Beier', 'Runolfsdottir', 'Brown', 'Wehner', 'Kozey', 'Walker', 'Sawayn', 'Bartoletti', 'Emmerich', 'Bode', 'Vandervort', 'Mayert', 'Hagenes', 'Ullrich', 'Deckow', 'Simonis', 'Heaney', 'Feeney', 'Altenwerth', 'Schowalter', 'Graham', 'Hudson', 'Paucek', 'Volkman', 'Wilkinson', 'Muller', 'Jones', 'Adams', 'Armstrong', 'Thompson', 'Nitzsche', 'Nicolas', 'Runte', 'Graham', 'Bernhard', 'Reilly', 'Huel', 'Williamson', 'Krajcik', 'Lang', 'Koelpin', 'Zieme', 'Becker', 'Herman', 'Herman', 'Crist', 'Kovacek', 'Grimes', 'Raynor', 'Hyatt', 'Braun', 'Emmerich', 'McDermott', 'Von', 'Roob', 'Purdy', 'Osinski', 'Bergstrom', 'Bayer', 'Kuhic',];
-var leadStatus = ['Open','Closed','In Progress'];
-var leadSource = ['Mobile App','Website','Branch','ATM','Email'];
-var leadType = ['Credit Card','Checking Account','Savings Account','Debit/Pre-Paid','Foreign Exchange'];
-
-
-
-
-
-
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getRandomDate(pastorfuture){
+function getRandomDate(pastorfuture, howmuch){
 	if (pastorfuture == 'future'){
-		return Date.today().add(getRandomInt(0,500)).day().toString('MM/dd/yyyy');
+		return Date.today().add(getRandomInt(0,howmuch)).day().toString('MM/dd/yyyy');
 	}
 	if (pastorfuture == 'past'){
-		return Date.today().add(-getRandomInt(0,500)).day().toString('MM/dd/yyyy');
+		return Date.today().add(-getRandomInt(0,howmuch)).day().toString('MM/dd/yyyy');
 	}
 }
 
@@ -42,14 +31,14 @@ function getRandomLeadData(){
 			creditScore: getRandomInt(550, 900),
 			assignedTo: '--',
 			source: getRandomArrayValue(leadSource),
-			lastContacted: getRandomDate('past'),
-			lastActivity: getRandomDate('past'),
+			lastContacted: getRandomDate('past',500),
+			lastActivity: getRandomDate('past',500),
 			products:'Credit, Checking',
 			}
 		);
 	}
 }
-function getRandomOpptyData(){
+function getRandomOpportunityData(){
 	var m = 2;
 	for (var i = 0; i < 100; i++) {
 		rbDB.insert("opportunity", 
@@ -64,8 +53,8 @@ function getRandomOpptyData(){
 			creditScore: getRandomInt(550, 900),
 			assignedTo: '--',
 			source: getRandomArrayValue(leadSource),
-			lastContacted: getRandomDate('past'),
-			lastActivity: getRandomDate('past'),
+			lastContacted: getRandomDate('past',500),
+			lastActivity: getRandomDate('past',500),
 			products:'Credit, Checking',
 			}
 		);
@@ -121,13 +110,14 @@ if(rbDB.isNew()){
 		creditScore: getRandomInt(550, 900),
 		assignedTo: '--',
 		source: 'Mobile',
-		lastContacted: getRandomDate('past'),
-		lastActivity: getRandomDate('past'),
+		lastContacted: getRandomDate('past',500),
+		lastActivity: getRandomDate('past',500),
 		products:'Credit, Checking',
 		}
 	];
 	// create the "opportunity" table
 	rbDB.createTableWithData('opportunity', rows);
+	getRandomOpportunityData();
 
 	// create the "lead" data
 	var rows = [
@@ -142,8 +132,8 @@ if(rbDB.isNew()){
 		creditScore: getRandomInt(550, 900),
 		assignedTo: '--',
 		source: 'Mobile',
-		lastContacted: getRandomDate('past'),
-		lastActivity: getRandomDate('past'),
+		lastContacted: getRandomDate('past',500),
+		lastActivity: getRandomDate('past',500),
 		products:'Credit, Checking',
 	}];
 	// create the "lead" table
@@ -197,11 +187,6 @@ if(rbDB.isNew()){
     // all create/drop/insert/update/delete operations should be committed
 	rbDB.commit();
 }
-
-
-var queryResult = rbDB.queryAll("client");
-
-var queryResult2 = rbDB.queryAll("lead");
  
 // select all financialAccount id's based on client ID
 var queryResult1 = rbDB.queryAll("xref", {
@@ -216,10 +201,6 @@ var queryResult1 = rbDB.queryAll("xref", {
 
 
 $(document).ready( function(){
-	queryResult.forEach(function(datum) {
-		$('#main-tab-content-details').prepend('<div>'+datum.uid+': '+datum.firstName+'</div>')
-	});
-
 	queryResult1.forEach(function(datum) {
 		$('#main-tab-content-details').prepend('<div>'+datum.pid+': '+datum.object+': '+datum.cid+'</div>')
 	});
@@ -230,6 +211,11 @@ $(document).ready( function(){
 
 /*  MODAL */
 $(document).ready( function(){
+
+if (!localStorage.getItem("navToggle")) {
+	// Set data
+	localStorage.setItem("navToggle", 'Open')
+}
 
     $(document).on('click', '.modal-action', function (){
     	var modalHeader, modalContent, modalFooter;
